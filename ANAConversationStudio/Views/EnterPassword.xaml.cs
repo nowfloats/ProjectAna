589be9fae63b2493760b543cedd6dc78e4bfa4e7
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2017-08-25 19:15:01 +0530

    Studio - Standalone build configurations added. Password reset option given.

--- ANAConversationStudio/Views/EnterPassword.xaml.cs
+++ ANAConversationStudio/Views/EnterPassword.xaml.cs
@@ -50,5 +50,17 @@ namespace ANAConversationStudio.Views
             if (e.Key == System.Windows.Input.Key.Enter)
                 Submit();
         }
+
+        private void ForgotPasswordClick(object sender, RoutedEventArgs e)
+        {
+            var msg = "Password protection is mainly set to secure the write access to your chat server which this studio has. \r\n\r\nIf you want to reset the password, your saved chat server connections will be deleted and you will have to configure them again. \r\n\r\nNote that your CHAT FLOWS WILL NOT BE DELETED, only the saved chat server connections will be deleted from this PC. \r\nYou just need to add the chat server connections again after you set the new password.\r\n\r\nDo you want to proceed?";
+            if (MessageBox.Show(msg, "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                Settings.Delete();
+
+                Process.Start(Application.ResourceAssembly.Location);
+                Application.Current.Shutdown();
+            }
+        }
     }
 }
