25f2e58a87094d41c980c550fd8bf3593fbc85e3
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2017-09-05 21:11:43 +0530

    NodeEditWindow Removed in favor of NodeEditorUserControl

---
+++ ANAConversationStudio/UIHelpers/NodeTypeToGroupVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using ANAConversationStudio.Models.Chat;
+using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ANAConversationStudio.UIHelpers
+{
+    public class NodeTypeToGroupVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var srcType = (NodeTypeEnum)value;
+            var visibleOnlyFor = (NodeTypeEnum)parameter;
+            return srcType == visibleOnlyFor ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
