c0f98fa3288be0b402d8fe7898528e26b32d12f0
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2017-10-22 01:07:35 +0530

    Branding Update finishing touches

--- ANAConversationSimulator/UIHelpers/DirectionToBorderColorConverter.cs
+++ ANAConversationSimulator/UIHelpers/DirectionToBorderColorConverter.cs
@@ -4,35 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace ANAConversationSimulator.UIHelpers
 {
-    public class DirectionToBorderColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value is MessageDirection direction)
-            {
-                switch (direction)
-                {
-                    case MessageDirection.In:
-                        return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 234, 237, 242)) { Opacity = 1 }; //Pale White
-                    case MessageDirection.AwkwardCenter:
-                        return new SolidColorBrush(Windows.UI.Colors.Transparent);
-                    case MessageDirection.Out:
-                        return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 173, 239)) { Opacity = 1 }; //NF Yellow
-                    default:
-                        return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 234, 237, 242)) { Opacity = 1 }; //Pale White
-                }
-            }
-            return value;
-        }
+	public class DirectionToBorderColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value is MessageDirection direction)
+			{
+				switch (direction)
+				{
+					case MessageDirection.In:
+						return (SolidColorBrush)App.Current.Resources["PaleWhiteBrush"];
+					case MessageDirection.AwkwardCenter:
+						return new SolidColorBrush(Windows.UI.Colors.Transparent);
+					case MessageDirection.Out:
+						return new SolidColorBrush((Color)App.Current.Resources["SystemAccentColor"]) { Opacity = 1 };
+					default:
+						return (SolidColorBrush)App.Current.Resources["PaleWhiteBrush"];
+				}
+			}
+			return value;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            return value;
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return value;
+		}
+	}
 }
