6364f4390f680ba2ff9efe358fe4b581c01a79cc
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2017-11-29 05:22:08 +0530

    electron app file menu added

--- src/electron.js
+++ src/electron.js
@@ -1,4 +1,4 @@
-﻿const { app, BrowserWindow } = require('electron')
+﻿const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -16,6 +16,97 @@ function createWindow() {
 	win.on('closed', () => {
 		win = null
 	})
+
+	const template = [
+		{
+			label: 'Edit',
+			submenu: [
+				{ role: 'undo' },
+				{ role: 'redo' },
+				{ type: 'separator' },
+				{ role: 'cut' },
+				{ role: 'copy' },
+				{ role: 'paste' },
+				{ role: 'pasteandmatchstyle' },
+				{ role: 'delete' },
+				{ role: 'selectall' }
+			]
+		},
+		{
+			label: 'View',
+			submenu: [
+				{ role: 'resetzoom' },
+				{ role: 'zoomin' },
+				{ role: 'zoomout' },
+				{ type: 'separator' },
+				{ role: 'togglefullscreen' }
+			]
+		},
+		{
+			role: 'window',
+			submenu: [
+				{ role: 'minimize' },
+				{ role: 'close' }
+			]
+		},
+		{
+			role: 'help',
+			submenu: [
+				{
+					label: 'About Ana',
+					click() { require('electron').shell.openExternal('http://ana.chat') }
+				}
+			]
+		}
+	]
+
+	if (process.platform === 'darwin') {
+		template.unshift({
+			label: app.getName(),
+			submenu: [
+				{ role: 'about' },
+				{ type: 'separator' },
+				{ role: 'services', submenu: [] },
+				{ type: 'separator' },
+				{ role: 'hide' },
+				{ role: 'hideothers' },
+				{ role: 'unhide' },
+				{ type: 'separator' },
+				{ role: 'quit' }
+			]
+		})
+
+		// Edit menu
+		template[1].submenu.push(
+			{ type: 'separator' },
+			{
+				label: 'Speech',
+				submenu: [
+					{ role: 'startspeaking' },
+					{ role: 'stopspeaking' }
+				]
+			}
+		)
+
+		// Window menu
+		template[3].submenu = [
+			{ role: 'close' },
+			{ role: 'minimize' },
+			{ role: 'zoom' },
+			{ type: 'separator' },
+			{ role: 'front' }
+		]
+	} else {
+		template.unshift({
+			label: 'File',
+			submenu: [
+				{ role: 'quit' }
+			]
+		})
+	}
+
+	const menu = Menu.buildFromTemplate(template)
+	Menu.setApplicationMenu(menu)
 }
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
